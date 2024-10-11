a28ae7faa1952cf05fcd189c027d8832ba626852
bullishvince <vincent@bullishlife.com>
2024-10-11 22:41:41 +0200

    add twitter-api dep + command to publish tweet

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,30 @@
 import * as vscode from "vscode";
+import { TwitterApi } from "twitter-api-v2";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
-    "vscode-tweet-publisher.publishTweet",
+    "vscode-tweet.publishTweet",
     async () => {
       const tweet = await vscode.window.showInputBox({
         prompt: "Enter your tweet",
       });
       if (tweet) {
-        // Logic to publish tweet will go here
-        vscode.window.showInformationMessage(`Tweet published: ${tweet}`);
+        try {
+          const client = new TwitterApi({
+            appKey: "YOUR_API_KEY",
+            appSecret: "YOUR_API_SECRET",
+            accessToken: "YOUR_ACCESS_TOKEN",
+            accessSecret: "YOUR_ACCESS_SECRET",
+          });
+
+          const rwClient = client.readWrite;
+          await rwClient.v2.tweet(tweet);
+          vscode.window.showInformationMessage(`Tweet published: ${tweet}`);
+        } catch (error: any) {
+          vscode.window.showErrorMessage(
+            "Failed to publish tweet: " + error.message
+          );
+        }
       } else {
         vscode.window.showErrorMessage("Tweet cannot be empty");
       }
